c274b34945a12e0066f8baa950b0493a4288a408
GeorgeWinner9E <Life.Winner@yandex.ru>
2019-12-15 17:54:00 +0300

    before making all universal

--- test_truant.php
+++ test_truant.php
@@ -16,9 +16,9 @@
 <body>
 
 <div class='container'>
-<button name="punish" ><img width=20  src='https://sc01.alicdn.com/kf/HTB1lLpcOjDpK1RjSZFrq6y78VXaE/bullwhip-bdsm-signal-single-tail-long-whip.jpg'> Наказать</button>
-<button name="reward"><img width=20  src='https://img1.freepng.ru/20180323/dqq/kisspng-gold-medal-olympic-medal-clip-art-golden-cup-5ab5a60f5a8812.1500798115218539673708.jpg'> Поощрить</button>
-<button name="nothing"><img width=20 src='https://c7.hotpng.com/preview/661/444/1011/american-red-cross-computer-icons-christian-cross-clip-art-big-red-cliparts-thumbnail.jpg'> Ничего не делать</button>
+<button name="punish" value='Наказать'><img width=20  src='https://sc01.alicdn.com/kf/HTB1lLpcOjDpK1RjSZFrq6y78VXaE/bullwhip-bdsm-signal-single-tail-long-whip.jpg'> Наказать</button>
+<button name="reward" value='Поощрить'><img width=20  src='https://img1.freepng.ru/20180323/dqq/kisspng-gold-medal-olympic-medal-clip-art-golden-cup-5ab5a60f5a8812.1500798115218539673708.jpg'> Поощрить</button>
+<button name="nothing" value='Ничего не делать'><img width=20 src='https://c7.hotpng.com/preview/661/444/1011/american-red-cross-computer-icons-christian-cross-clip-art-big-red-cliparts-thumbnail.jpg'> Ничего не делать</button>
 </div>
 <script src="test_truant_script.js"></script>
 
--- test_truant_script.js
+++ test_truant_script.js
@@ -1,88 +1,63 @@
-// начальное состояние кнопки — не кликнута
-// javascript здесь нашёл и выбрал элемент кнопки
-var killbutton = document.getElementsByTagName('button')[0];
-var savebutton = document.getElementsByTagName('button')[1];
-var nothingbutton = document.getElementsByTagName('button')[2];
-var time = document.createElement('div');
-var day = document.createElement('div');
-var n=1;
-var max=20;
-var min=-20;
-var itime=900;
-var stime = '900';
-var random_number = 0;
 
+
+//Шаблон: var buttonname = document.getElementsByTagName('button')['имя кнопки на странице html']
+var punishbutton = document.getElementsByTagName('button')['punish']; //установка соответствия между кнопками на странице и кодом js
+var rewardbutton = document.getElementsByTagName('button')['reward'];
+var nothingbutton = document.getElementsByTagName('button')['nothing'];
+
+var time = document.createElement('div'); //Поле "Время и события"
 time.className = "alert";
+var day = document.createElement('div'); //Поле "День"
+var Iday=1; //Счетчик дней
+var max=20; //Максимальное время опоздания
+var min=-20; //Минимальное время опоздания (По умолчанию отрицательно)
 
-random_number = Math.random()*(max-min)+min; 
+
+function NewDay(Saction){   //Функция нового дня
+    let random_number = 0;
+    let itime=900;
+    let stime = '900';
+    random_number = Math.random()*(max-min)+min; //Генерируем время прихода Васи 
      itime = 900+random_number;
      if (itime<900){
          itime=itime-40;
      }
      stime = itime.toString();
 
-     time.innerHTML = stime[0]+':'+stime[1]+stime[2];
-     day.innerHTML = '<strong>День '+n+'</strong>';
-     n=n+1;
+     if (Iday==1){             //Создаем блоки div с нужной информацией
+     time.innerHTML = 'Сегодня Вася пришел в <strong>'+stime[0]+':'+stime[1]+stime[2]+'</strong><br>Как Вы поступите?<br>(Выберите один из вариантов)';
+     day.innerHTML = '<strong>День '+Iday+'</strong>';}
+     else{
+     time.innerHTML = 'Вчера Вы выбрали: '+Saction+'<br>Сегодня Вася пришел в <strong>'+stime[0]+':'+stime[1]+stime[2]+'</strong><br>Как Вы поступите?<br>';
+     day.innerHTML = '<strong>День '+Iday+'</strong>';}         
      
-     document.body.prepend(time);
-     time.after(day);
+     document.body.prepend(day);  //Выводим информацию на страницу
+     day.after(time);
 
-savebutton.onclick = function() {
-    if (n==31){
-        window.close("test_truant.php");
-    window.open("anketa_truant.php");
-    }
-     random_number = Math.random()*(max-min)+min; 
-     itime = 900+random_number;
-     if (itime<900){
-         itime=itime-40;
-     }
-     stime = itime.toString();
+     Iday=Iday+1; //увеличиваем счетчик дня
 
-     time.innerHTML = stime[0]+':'+stime[1]+stime[2];
-     day.innerHTML = '<strong>День '+n+'</strong>';
-     n=n+1;
-     
-     document.body.prepend(time);
-     time.after(day);
+}
 
-  };
+NewDay();
   
-killbutton.onclick = function() {
-    if (n==31){
-        window.close("test_truant.php");
-    window.open("anketa_truant.php");
-    }
-    random_number = Math.random()*(max-min)+min;
-    itime = 900+random_number;
-    if (itime<900){
-        itime=itime-40;
-    }
-    stime = itime.toString();
+/*Функции для действий ползователя 
+Шаблон
+buttonname.onclick = function(){  //В value должно быть название действия на русском
+    NewDay(buttonname.value);  //value необходимо задавать при создании кнопки в test_truant.php  
+}*/
 
-    time.innerHTML = stime[0]+':'+stime[1]+stime[2];
-    day.innerHTML = '<strong>День '+n+'</strong>';
-    n=n+1;
-    document.body.prepend(time);
-    time.after(day);
+punishbutton.onclick = function() {
+    NewDay(punishbutton.value);
 };
 
-nothingbutton.onclick = function() {
-    if (n==31){
+rewardbutton.onclick = function() {
+    /*if (Iday==31){
         window.close("test_truant.php");
     window.open("anketa_truant.php");
-    }
-    random_number = Math.random()*(max-min)+min;
-    itime = 900+random_number;
-    if (itime<900){
-        itime=itime-40;
-    }
-    stime = itime.toString();
+    }*/
+     NewDay(rewardbutton.value);
+  };
 
-    time.innerHTML = stime[0]+':'+stime[1]+stime[2];
-    day.innerHTML = '<strong>День '+n+'</strong>';
-    n=n+1;
-    document.body.prepend(time);
-    time.after(day);
+nothingbutton.onclick = function() {
+    NewDay(nothingbutton.value);
 };
