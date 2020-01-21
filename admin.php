654d45bc1b5491b752b83b0d8e8b6831800f6d79
GeorgeWinner9e <life.winner@yandex.ru>
2020-01-21 19:46:09 +0300

    admin files

---
+++ admin.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<!--<link rel="stylesheet"  href="admin.css">-->
+<title>Прогульщик Вася (Админский доступ)</title>
+  <style>
+   body {
+    background: lightblue; /* Цвет фона */
+    /* color: #fc0; /* Цвет текста */
+    font-size: 30px;
+   }
+
+  </style>
+</head>
+<body>
+
+
+
+</body>
+
+</html>
