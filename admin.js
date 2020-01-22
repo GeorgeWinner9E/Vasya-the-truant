aca3885cd24985ced7f6ae86cf34d665e04b2a1a
GeorgeWinner9e <life.winner@yandex.ru>
2020-01-22 20:09:09 +0300

    json show

--- admin.js
+++ admin.js
@@ -1,25 +1,50 @@
 window.onload= function() {
 
-    
+    let stratslist;
+    let strats;
+    let $strlist = $('#strlist');
+    let $table = $('#strpanel');
+    let $add = $('#addbtn');
 
-    var $table = $('#strpanel');
-    var $add = $('#addbtn');
-    $add.click(function AddRow() {
-    var $Row = $($table[0].insertRow());
+    $.getJSON('strategies.json', function(json) {
+        stratslist = json.AllStrategies;
+        strats = json;
+        for (let i=0; i<Object.keys(stratslist).length; i++) {
+            let $str = $('<option value="'+i+'">'+stratslist[i]+'</option>');
+            $strlist.append($str);
+        }
 
-    for (let i=0; i<3; i++){
-    let    $Cell = $($Row[0].insertCell());
-    let $input = $('<input type="text">');
-    $Cell.append($input);}
+        loadstr();
+    });
 
-    let    $Cell = $($Row[0].insertCell());
-    let $delbtn = $('<input type="button">');
-    $delbtn.click(function DelRow() {
+    $add.click(AddRow);
+
+    function AddRow() {
+        let $Row = $($table[0].insertRow());
+        for (let i=0; i<3; i++){
+            let    $Cell = $($Row[0].insertCell());
+            let $input = $('<input type="text">');
+            $Cell.append($input);}
+
+        let $Cell = $($Row[0].insertCell());
+        let $delbtn = $('<input type="button" value="X">');
+        $delbtn.click(function DelRow() {
             $table[0].deleteRow($(this).closest("tr").index());
         })
-    $Cell.append($delbtn);
+        $Cell.append($delbtn);
+    }
 
-    })
 
+
+    function loadstr() {
+        let chosenone = strats[$strlist.val()];
+        for (let i=0; i<Object.keys(chosenone).length; i++){
+            AddRow();
+            for (let j=0; j<3; j++) {
+                $table.children().children().children()[j].children("input").value = chosenone[i][j];
+
+
+            }}
+    }
 
 }
